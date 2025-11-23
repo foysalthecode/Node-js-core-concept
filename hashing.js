6ce41c9b914d1eee3c67da9f5461687d9f740d47
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-23 23:07:16 +0600

    practice: sha256 and sha512 hash

--- hashing.js
+++ hashing.js
@@ -8,3 +8,15 @@ const md5Hash2 = crypto.createHash("md5").update("password123").digest("hex"); /
 console.log("input: password123");
 console.log("MD5 HashedPassword: ", md5Hash);
 console.log("MD5 Hashed2 : ", md5Hash2);
+
+console.log("-".repeat(50));
+
+const sha256Hash = crypto.createHash("sha256").update("password").digest("hex");
+console.log("input: password123");
+console.log("sha256 HashedPassword: ", sha256Hash);
+
+console.log("-".repeat(50));
+
+const sha512Hash = crypto.createHash("sha512").update("password").digest("hex");
+console.log("input: password123");
+console.log("sha512 HashedPassword: ", sha512Hash);
